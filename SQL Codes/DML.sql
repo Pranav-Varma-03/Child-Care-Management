407f9aa6cf73a864b1492bbcf0bed9fe6afc2a15
Pranav-Varma-03 <harshapranav323@gmail.com>
2023-11-01 22:38:34 +0530

    updated SQL Codes

---
+++ SQL Codes/DML.sql
@@ -0,0 +1,7 @@
+use CMS;
+-- INSERT INTO SystemAdmin (name, email, password)
+-- VALUES ('Krishna', 'krishna323@gmail.com', '123');
+
+SELECT * FROM SystemAdmin WHERE email = 'krishna323@gmail.com';
+
+-- DELETE FROM SystemAdmin WHERE id = 3;
